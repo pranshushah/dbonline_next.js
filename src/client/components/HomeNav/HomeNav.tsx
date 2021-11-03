de10ab4d38983fb6ee7040f3078df3ac893e1af5
pranshu shah <pranshu.shah23@gmail.com>
2021-11-03 19:21:10 +0530

    signin removed

--- src/client/components/HomeNav/HomeNav.tsx
+++ src/client/components/HomeNav/HomeNav.tsx
@@ -4,20 +4,20 @@ import NavbarContainer from '../UI/Navbar/NavContainer';
 import NavLogo from '../UI/Navbar/NavLogo/NavLogo';
 import NavLinksContainer from '../UI/Navbar/NavLinksContainer/NavLinksContainer';
 import '../../utils/Types';
-import { useRecoilValue } from 'recoil';
-import { authenticatedAtom } from '../../atoms/authenticatedAtom';
+// import { useRecoilValue } from 'recoil';
+// import { authenticatedAtom } from '../../atoms/authenticatedAtom';
 
 function Nav() {
-  const authenticated = useRecoilValue(authenticatedAtom);
+  // const authenticated = useRecoilValue(authenticatedAtom);
   return (
     <NavbarContainer dark>
       <NavLogo text='DB ONLINE' />
       <NavLinksContainer>
-        <NavLink
+        {/* <NavLink
           text={authenticated ? 'Log out' : 'Sing in'}
           link={authenticated ? '/logout' : '/api/auth/google'}
           dark
-        />
+        /> */}
         <NavLink text='Dashboard' link='/dashboard' dark />
       </NavLinksContainer>
     </NavbarContainer>
